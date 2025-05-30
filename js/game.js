e3331d8a3c3d888f4740c8a061d39b7c6c4b8930
Oven29 <ovchinno.ivan@gmail.com>
2025-05-31 00:49:18 +0300

    [edit] - Edit generate map, add unit hp

--- js/game.js
+++ js/game.js
@@ -1,5 +1,6 @@
 import { BaseTile, EnemyTile, HeroTile } from "./tiles.js";
 import { generateMap } from "./map.js";
+import cst from "./constants.js";
 
 export class Game {
     init() {
@@ -11,13 +12,13 @@ export class Game {
         this.enemies = enemies.map(({ x, y }) => (
             new EnemyTile().draw(x, y)
         ));
-        this.hero = new HeroTile().draw(hero);
+        this.hero = new HeroTile().draw(hero.x, hero.y);
 
         for (let i = 0; i < this.map.length; i++) {
             for (let j = 0; j < this.map[i].length; j++) {
                 const tile = this.map[i][j];
                 new BaseTile(tile.type).draw(j, i);
-                if (tile.object) {
+                if ([cst.TILE_POTION, cst.TILE_SWORD].includes(tile.object)) {
                     new BaseTile(tile.object).draw(j, i);
                 }
             }
--- js/tiles.js
+++ js/tiles.js
@@ -1,3 +1,5 @@
+import cst from "./constants.js";
+
 export class BaseTile {
     constructor(className) {
         this.className = className;
@@ -18,31 +20,48 @@ export class BaseTile {
 }
 
 export class UnitTile extends BaseTile {
-    constructor(hp) {
+    constructor(className, hp) {
+        super(className);
         this.hp = hp;
-    }    
+        this.$hp = $('<div class="health" style="width: 100%"></div>');
+    }
 
     draw(x, y) {
+        this.$tile.append(this.$hp);
         super.draw(x, y);
+        console.log('Draw', this.className);
+        return this;
     }
 
     move(x, y) {
-
+        this.x = x;
+        this.y = y;
+        this.$tile.attr('style', `left: calc(${this.x} * var(--tile-width)); top: calc(${this.y} * var(--tile-height));`);
     }
 
-    setHP() {
+    setHP(hp) {
+        this.hp = hp;
+        this.$hp.css('width', `${this.hp / cst.HERO_HP * 100}%`);
+    }
 
+    takeDamage(damage) {
+        this.setHP(this.hp - damage);
+        if (this.hp <= 0) {
+            this.remove();
+            return true;
+        }
+        return false;
     }
 }
 
 export class HeroTile extends UnitTile {
     constructor() {
-        super('tileH');
+        super(cst.TILE_HERO, cst.HERO_HP);
     }
 }
 
 export class EnemyTile extends UnitTile {
     constructor() {
-        super('tileE');
+        super(cst.TILE_ENEMY, cst.ENEMY_HP);
     }
 }
